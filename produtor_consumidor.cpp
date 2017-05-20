d30e4b001c1f88ea2f8ce4369aeb79df02fa427b
joabegregorio <joabe3004@gmail.com>
2017-05-20 12:45:18 -0300

    Update produtor_consumidor.cpp

--- produtor_consumidor.cpp
+++ produtor_consumidor.cpp
@@ -1,12 +1,33 @@
+#include <semaphore.h>
 int buffer[1];
 sem_t cheio,vazio;
 
-void produtor (void*){
-    while(){
-        item = produceitem();
+void produtor(void *) {
+    while (true) {
+        item = 1;
         sem_wait(&vazio);
-        putitemintobuffer(item)
+        buffer = item;
+        sem_post(&cheio);
     }
+}
 
 
+void consumidor(void *) {
+    while (true) {
+        sem_wait(&cheio);
+        item = 0;
+        sem_post(&vazio);
+        buffer = item;
+    }
+}
+
+
+int main(){
+    pthread_t prod, cons;
+    sem_init(&cheio, 0, ?);
+    sem_init(&vazio, 0, ?);
+    pthread_create(&prod, NULL, produtor, NULL);
+    pthread_create(&cons, NULL, consumidor, NULL);
+    pthread_join(prod, NULL);
+    pthread_join(cons, NULL);
 }
